8a5b714c565f435ed146fd6ef41bc825f73e1bee
Andrey Artemenko <a.artemenko@jetmoney.me>
2017-04-13 11:53:37 +0300

    first commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'SodexoPass' do
+  use_frameworks!
+
+  pod 'Alamofire'
+  pod 'SwiftyJSON'
+  pod 'Eureka'
+end
---
+++ SodexoPass/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SodexoPass
+//
+//  Created by Andrey Artemenko on 13/04/2017.
+//  Copyright © 2017 Andrey Artemenko. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class ViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
